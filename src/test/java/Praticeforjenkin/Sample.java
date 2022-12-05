45d1e5ed8a99de03469060b64ced1bcbc79d0c95
realme <realme@SURAJARMAN>
2022-12-05 16:15:21 +0530

    Test5

---
+++ src/test/java/Praticeforjenkin/Sample.java
@@ -0,0 +1,14 @@
+package Praticeforjenkin;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+@Test
+public void create() {
+	System.out.println("Build creation");
+}
+@Test
+public void update() {
+	System.out.println("Update creation");
+}
+}
---
+++ src/test/java/Praticeforjenkin/sample2.java
@@ -0,0 +1,14 @@
+package Praticeforjenkin;
+
+import org.testng.annotations.Test;
+
+public class sample2 {
+	@Test
+public void Sample() {
+	System.out.println("script 1");
+}
+	@Test	
+public void sample2() {
+	System.out.println("script2");
+}
+}
